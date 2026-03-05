81913a71ece54380426a9c35b9e10b3808e0f25b
Billy Arante <arantebillywilson@gmail.com>
2026-03-05 21:36:01 +0800

    feat: ✨ token metadata

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ def main():
     except Exception:
         raise RuntimeError("API key is not found")
     client = genai.Client(api_key=api_key)
+    prompt_text = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=prompt_text,
     )
-    print(response.text)
+    print(f"User prompt: {prompt_text}")
+    if response.usage_metadata:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
