7b8ca31ef22b13fbcd082afc944f83aad17e0dc4
Billy Arante <arantebillywilson@gmail.com>
2026-03-09 09:41:56 +0800

    feat: ✨ user input

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
     load_dotenv()
+    parser = argparse.ArgumentParser(description="Ghinton Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     try:
         api_key = os.environ.get("GEMINI_API_KEY")
     except Exception:
         raise RuntimeError("API key is not found")
     client = genai.Client(api_key=api_key)
-    prompt_text = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=prompt_text,
+        contents=args.user_prompt,
     )
-    print(f"User prompt: {prompt_text}")
+    print(f"User prompt: {args.user_prompt}")
     if response.usage_metadata:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
