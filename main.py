3d347cc9c21238d727a0a233aa31a112bf4b3808
Billy Arante <arantebillywilson@gmail.com>
2026-03-18 23:51:13 +0800

    feat: ✨ verbose output

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     load_dotenv()
     parser = argparse.ArgumentParser(description="Ghinton Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     try:
         api_key = os.environ.get("GEMINI_API_KEY")
@@ -21,10 +22,11 @@ def main():
         model="gemini-2.5-flash",
         contents=messages,
     )
-    print(f"User prompt: {args.user_prompt}")
-    if response.usage_metadata:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
 
 
