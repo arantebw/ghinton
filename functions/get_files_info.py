63e81d7a2d54788704f78c3742d1ab2c2af116fa
Billy Arante <arantebillywilson@gmail.com>
2026-03-20 23:45:44 +0800

    feat: ✨ get files - step 5

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,19 +2,22 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.join(working_dir_abs, directory)
-    valid_target_dir = (
-        os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-    )
-    if not valid_target_dir:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(target_dir):
-        return f'Error: "{directory}" is not a directory'
-    lines = []
-    for name in os.listdir(target_dir):
-        path = os.path.join(target_dir, name)
-        lines.append(
-            f"- {name}: file_size={os.path.getsize(path)} bytes, is_dir={os.path.isdir(path)}"
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.join(working_dir_abs, directory)
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         )
-    return "\n".join(lines)
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        lines = []
+        for name in os.listdir(target_dir):
+            path = os.path.join(target_dir, name)
+            lines.append(
+                f"- {name}: file_size={os.path.getsize(path)} bytes, is_dir={os.path.isdir(path)}"
+            )
+        return "\n".join(lines)
+    except Exception as e:
+        return f"Error: {e}"
