dd804398d6029df70078eb4490ae7892b7c30f62
Billy Arante <arantebillywilson@gmail.com>
2026-03-20 23:41:04 +0800

    feat: ✨ get files - step 3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,3 +9,5 @@ def get_files_info(working_directory, directory="."):
     )
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
