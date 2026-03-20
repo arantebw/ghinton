14a6ca24b2a39357d2e6625dd7877faf098d1fb2
Billy Arante <arantebillywilson@gmail.com>
2026-03-20 23:55:22 +0800

    feat: ✨ get files - step 6 and 7

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,7 +4,7 @@ import os
 def get_files_info(working_directory, directory="."):
     try:
         working_dir_abs = os.path.abspath(working_directory)
-        target_dir = os.path.join(working_dir_abs, directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
         valid_target_dir = (
             os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         )
